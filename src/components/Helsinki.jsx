d414c65a502674fb3bc4bd4c2f02209752760bfc
LevPewPew <levthedev@protonmail.com>
2020-07-03 08:51:58 +1000

    basic leaflet map working

--- src/components/Helsinki.jsx
+++ src/components/Helsinki.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import Leaflet from 'leaflet';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import styled from 'styled-components';
 import 'leaflet/dist/leaflet.css';
 
 const Root = styled(Map)`
-  height: 400px;
-  width: 100%;
+  height: 500px;
+  width: 500px;
 `;
+
+const marker = new Leaflet.Icon({
+  iconUrl: 'https://maps.google.com/mapfiles/kml/pushpin/red-pushpin.png' ,
+  iconSize: [64, 64],
+  iconAnchor: [20, 62]
+});
 
 // must be a Class component, react-leaflet refuses to work as a functional
 // component. i don't know why.
 class Helsinki extends React.Component {
   state = {
-    center: [51.505, -0.091],
+    center: [60.192059, 24.945831],
     zoom: 13,
   };
 
@@ -20,10 +27,11 @@ class Helsinki extends React.Component {
     return (
       <Root center={this.state.center} zoom={this.state.zoom}>
         <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          attribution={'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'}
+          url={'https://cdn.digitransit.fi/map/v1/hsl-map-256/{z}/{x}/{y}.png'}
         />
-        <Marker position={this.state.center}>
+        <Marker icon={marker} position={this.state.center}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
