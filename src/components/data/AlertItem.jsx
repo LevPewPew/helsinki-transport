4abafc47c0495e8b91ebaa90e64a3b4c73b93a3b
LevPewPew <levthedev@protonmail.com>
2020-07-19 08:24:20 +1000

    made AlertItem codes translate to nice human text

--- src/components/data/AlertItem.jsx
+++ src/components/data/AlertItem.jsx
@@ -41,10 +41,107 @@ function AlertItem ({ item }) {
       alertColor = 'white';
   }
 
+  let alertEffect;
+  switch(item.alertEffect) {
+    case 'NO_SERVICE':
+      alertEffect = 'No service';
+      break;
+    case 'REDUCED_SERVICE':
+      alertEffect = 'Reduced service';
+      break;
+    case 'SIGNIFICANT_DELAYS':
+      alertEffect = 'Significant delays';
+      break;
+    case 'DETOUR':
+      alertEffect = 'Detour';
+      break;
+    case 'ADDITIONAL_SERVICE':
+      alertEffect = 'Additional service';
+      break;
+    case 'MODIFIED_SERVICE':
+      alertEffect = 'Modified service';
+      break;
+    case 'OTHER_EFFECT':
+      alertEffect = null;
+      break;
+    case 'UNKNOWN_EFFECT':
+      alertEffect = null;
+      break;
+    case 'STOP_MOVED':
+      alertEffect = 'Stop has moved';
+      break;
+    case 'NO_EFFECT':
+      alertEffect = null;
+      break;
+    default:
+      alertEffect = 'Possible change in service or operating procedures';
+  }
+
+  let alertCause;
+  switch(item.alertCause) {
+    case 'UNKNOWN_CAUSE':
+      alertCause = 'an unknown cause';
+      break;
+    case 'OTHER_CAUSE':
+      alertCause = null;
+      break;
+    case 'TECHNICAL_PROBLEM':
+      alertCause = 'technical problems';
+      break;
+    case 'STRIKE':
+      alertCause = 'strike';
+      break;
+    case 'DEMONSTRATION':
+      alertCause = 'demonstration';
+      break;
+    case 'ACCIDENT':
+      alertCause = 'accident';
+      break;
+    case 'HOLIDAY':
+      alertCause = 'public holiday';
+      break;
+    case 'WEATHER':
+      alertCause = 'weather';
+      break;
+    case 'MAINTENANCE':
+      alertCause = 'maintenence';
+      break;
+    case 'CONSTRUCTION':
+      alertCause = 'construction';
+      break;
+    case 'POLICE_ACTIVITY':
+      alertCause = 'police activity';
+      break;
+    case 'MEDICAL_EMERGENCY':
+      alertCause = 'medical emergency';
+      break;
+    default:
+      alertCause = null;
+  }
+
   const convertEpochToHumanTime = (epochTime) => {
     let d = new Date(0);
     d.setUTCSeconds(epochTime);
     return d.toLocaleString('en-GB', { timeZone: 'Europe/Helsinki' });
+  };
+
+  const alertCauseAndEffectMessageBuilder = (alertEffect, alertCause) => {
+    let message;
+    if (alertEffect == null) {
+      if (alertCause == null) {
+        message = "Possible change in service or operating procedures"
+      } else {
+        message = `Possible change in service or operating procedures due to ${alertCause}`
+      }
+    } else {
+      if (alertCause == null) {
+        message = `${alertEffect}`
+      } else {
+        message = `${alertEffect} due to ${alertCause}`
+      }
+    }
+    
+    return message;
   };
 
   return (
@@ -57,7 +154,7 @@ function AlertItem ({ item }) {
         <IoMdAlert
           color={alertColor}
         />
-        <p>{item.alertEffect} due to {item.alertCause}</p>
+        <p><em>{alertCauseAndEffectMessageBuilder(alertEffect, alertCause)}</em></p>
       </div>
       <div className="timestamp-container">
         <p>Begins:</p>
