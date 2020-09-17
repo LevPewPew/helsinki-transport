6ec093fb7a8c94b49883d5e56652b889579de653
LevPewPew <levthedev@protonmail.com>
2020-09-17 17:53:47 +1000

    refactor to use render props and delete specific components

--- src/components/DataList.jsx
+++ src/components/DataList.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import GridLoader from 'react-spinners/GridLoader';
-import styled from 'styled-components';
-import { COLORS } from 'styles';
+import React from "react";
+import GridLoader from "react-spinners/GridLoader";
+import styled from "styled-components";
+import { COLORS } from "styles";
 
 const Root = styled.div`
   height: 100%;
@@ -15,21 +15,36 @@ const Root = styled.div`
   }
 `;
 
-const DataList = ({ children, error, loading }) => (
-  <Root>
-    <div className="spinner-container" style={{display: loading ? 'flex' : 'none'}}>
-      <GridLoader
-        size={50}
-        color={COLORS.SELECTED_DETAILS}
-        loading={loading}
-      />
-    </div>
-    {
-      error ?
-      <div>Data Query Error: {error}</div> :
-      children
-    }
-  </Root>
-);
+const DataList = ({ children, query, animate }) => {
+  const { loading, error, data } = useQuery(query);
+
+  return (
+    <Root>
+      <div
+        className="spinner-container"
+        style={{ display: loading ? "flex" : "none" }}
+      >
+        <GridLoader
+          size={50}
+          color={COLORS.SELECTED_DETAILS}
+          loading={loading}
+        />
+      </div>
+      {error ? (
+        <div>Data Query Error: {error}</div>
+      ) : (
+        <>
+          <DataList error={error} loading={loading}>
+            {data?.routes.map((item, i) => (
+              <DataListItemCard animate={animate}>
+                <RouteItem item={item} key={`RouteItem${i}`} />
+              </DataListItemCard>
+            ))}
+          </DataList>
+        </>
+      )}
+    </Root>
+  );
+};
 
 export default DataList;
